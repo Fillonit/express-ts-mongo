cf5ad3d801454466fddcc0feaf6af446287fa98d
Fillonit Ibishi <filloniti@gmail.com>
2023-08-03 19:18:44 +0200

    user routes/controllers

---
+++ src/controllers/users.ts
@@ -0,0 +1,64 @@
+import express from "express";
+
+import { getUsers, deleteUserById, getUserById } from "../db/users";
+
+export const getAllUsers = async (
+  req: express.Request,
+  res: express.Response
+) => {
+  try {
+    const users = await getUsers();
+
+    return res.status(200).json({ users }).end();
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const deleteUser = async (
+  req: express.Request,
+  res: express.Response
+) => {
+  try {
+    const { id } = req.params;
+
+    const deletedUser = await deleteUserById(id);
+
+    return res
+      .status(200)
+      .json({ message: "User deleted successfully", user: deletedUser })
+      .end();
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const updateUser = async (
+  req: express.Request,
+  res: express.Response
+) => {
+  try {
+    const { id } = req.params;
+    const { username, email, password } = req.body;
+
+    if (!username) {
+      return res.status(400).json({ message: "Username is required" });
+    }
+
+    const user = await getUserById(id);
+
+    user.username = username;
+
+    await user.save();
+
+    return res
+      .status(200)
+      .json({ message: "User updated successfully", user: user })
+      .end();
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,3 +1,55 @@
 import express from "express";
+import { get, merge } from "lodash";
 
-// todo
+import { getUserBySessionToken } from "../db/users";
+
+export const isAuthenticated = async (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  try {
+    const sessionToken = req.cookies.sessionToken;
+
+    if (!sessionToken) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    const user = await getUserBySessionToken(sessionToken);
+
+    if (!user) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    merge(req, { identity: user });
+
+    return next();
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({ error: error.message });
+  }
+};
+
+export const isOwner = async (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  try {
+    const identity = get(req, "identity._id") as string;
+    const { id } = req.params;
+
+    if (!identity) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    if (identity.toString() !== id) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    return next();
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({ error: error.message });
+  }
+};

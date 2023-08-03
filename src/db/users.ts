e78f1df26e8687a025e7f5d8fc1708c64a15c4fa
Fillonit Ibishi <filloniti@gmail.com>
2023-08-03 10:46:42 +0200

    User Model

--- src/db/users.ts
+++ src/db/users.ts
@@ -9,3 +9,22 @@ const UserSchema = new mongoose.Schema({
     sessionToken: { type: String, selected: false },
   },
 });
+
+export const UserModel = mongoose.model("User", UserSchema);
+
+export const getUsers = () => UserModel.find();
+export const getUsersByEmail = (email: string) => UserModel.findOne({ email });
+
+export const getUserBySessionToken = (sessionToken: string) =>
+  UserModel.findOne({ "authentication.sessionToken": sessionToken });
+
+export const getUserById = (id: string) => UserModel.findById(id);
+
+export const createUser = (values: Record<string, any>) =>
+  new UserModel(values).save().then((user) => user.toObject());
+
+export const updateUser = (id: string, values: Record<string, any>) =>
+  UserModel.findByIdAndUpdate(id, values).then((user) => user.toObject());
+
+export const deleteUserById = (id: string) =>
+  UserModel.findOneAndDelete({ _id: id });

d275d42100a398872a804d71eb261c21ec6f0a04
Fillonit Ibishi <filloniti@gmail.com>
2023-08-03 13:16:43 +0200

    Login/Auth

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -1,7 +1,51 @@
 import express from "express";
 
-import { getUsersByEmail, createUser } from "db/users";
+import { getUsersByEmail, createUser } from "../db/users";
 import { authentication, random } from "../helpers";
+
+export const login = async (req: express.Request, res: express.Response) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({ message: "Missing fields" });
+    }
+
+    const user = await getUsersByEmail(email).select(
+      "+authentication.salt +authentication.password"
+    );
+
+    if (!user) {
+      return res.status(400).json({ message: "User not found" });
+    }
+
+    const expectedHash = authentication(user.authentication.salt, password);
+
+    if (expectedHash !== user.authentication.password) {
+      return res.status(403).json({ message: "Invalid credentials" });
+    }
+
+    const salt = random();
+    user.authentication.sessionToken = authentication(
+      salt,
+      user._id.toString()
+    );
+
+    await user.save();
+
+    res.cookie("sessionToken", user.authentication.sessionToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      domain: "localhost",
+      path: "/",
+    });
+
+    return res.status(200).json({ user }).end();
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
 
 export const register = async (req: express.Request, res: express.Response) => {
   try {

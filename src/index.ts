e2e6fec3fc5e5c8d5b5b3304d3348cedc1248ccc
Fillonit Ibishi <filloniti@gmail.com>
2023-08-08 21:36:44 +0200

    some stuff

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 
+let pkg = require("../package.json");
 import router from "./router";
 
 dotenv.config();
@@ -44,11 +45,11 @@ app.use("/", router());
 app.get("/", (req: express.Request, res: express.Response) => {
   res.status(200).json({
     "vue-api": {
-      _desc: "This is the api for my full-stack vue project.",
-      _repo: "https://github.com/Fillonit/vue-api",
-      _author: "Fillonit",
-      _version: "1.0.0",
-      _license: "MIT",
+      _desc: pkg.description,
+      _repo: pkg.repo,
+      _author: pkg.author,
+      _version: pkg.version,
+      _license: pkg.license,
     },
   });
 });
